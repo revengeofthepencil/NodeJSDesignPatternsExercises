60a2194203a583238c382c1f321c31ca4ac8816a
Alex Walker <awalker@benaroyaresearch.org>
2023-12-28 20:14:18 -0800

    initial commit

---
+++ ch03/ex3_1_simpleEvent.js
@@ -0,0 +1,41 @@
+import { EventEmitter } from 'events'
+import { readFile } from 'fs'
+class FindRegex extends EventEmitter {
+  constructor (regex) {
+    super()
+    this.regex = regex
+    this.files = []
+  }
+  addFile (file) {
+    this.files.push(file)
+    return this
+  }
+  find () {
+    process.nextTick(() => this.emit('matchstart', this.files) );
+    for (const file of this.files) {
+      readFile(file, 'utf8', (err, content) => {
+        if (err) {
+          return this.emit('error', err)
+        }
+        this.emit('fileread', file)
+        const match = content.match(this.regex)
+        if (match) {
+          match.forEach(elem => this.emit('found', file, elem))
+        }
+      })
+    }
+    return this
+  }
+}
+
+const findRegexInstance = new FindRegex(/hello \w+/);
+findRegexInstance
+  .addFile('exfiles/fileA.txt')
+  .addFile('exfiles/fileB.json')
+  .addFile('exfiles/file_does_not_exist.json')
+  .find()
+  .on('matchstart', files => console.log(`Searching ${files.length} files`))
+  .on('fileread', file => console.log(`${file} was read`))
+  .on('found', (file, match) => console.log(`Matched "${match}" in ${file}`))
+  .on('error', err => console.log(`Error emitted ${err.message}`))
+
