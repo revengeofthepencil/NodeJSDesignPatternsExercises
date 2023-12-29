b9ad64df81db6d0b010c1ebe44a2185782056fbe
Alex Walker <awalker@benaroyaresearch.org>
2023-12-28 20:15:35 -0800

    move interval to constant

--- ch03/ex3_2_ticker.js
+++ ch03/ex3_2_ticker.js
@@ -9,25 +9,26 @@ as the result, the total count of tick events emitted. Hint: you
 can use setTimeout() to schedule another setTimeout() recursively.
 */
 
+const INTERVAL_MS = 50;
+
 
 const ticker = (maxMs, callback) => {
 	const eventEmitter = new EventEmitter()
 	let countTicks = 0;
-	const intervalMS = 50;
 	let totalMS = 0;
 
 	const tickThatThing = () => {
 		if (totalMS < maxMs) {
 			countTicks += 1;
-			totalMS += intervalMS;
+			totalMS += INTERVAL_MS;
 			eventEmitter.emit('tick', totalMS);
-			setTimeout(tickThatThing, intervalMS);
+			setTimeout(tickThatThing, INTERVAL_MS);
 		} else {
 			callback(countTicks);
 		}
 	};
 
-	setTimeout(tickThatThing, intervalMS);
+	setTimeout(tickThatThing, INTERVAL_MS);
 	return eventEmitter;
 };
 
