f1a9748f7a3b2cbe5aea8d31db7c38fc947766de
Alex Walker <awalker@benaroyaresearch.org>
2024-11-15 15:50:40 -0800

    use regex class

--- ch03/ex01.js
+++ ch03/ex01.js
@@ -2,34 +2,43 @@
 3.1 A simple event: Modify the asynchronous FindRegex class so
 that it emits an event when the find process starts, passing the
 input files list as an argument. Hint: beware of Zalgo!
-*/
-
-import { EventEmitter } from 'events';
+*/import { EventEmitter } from 'events';
 import { readFile } from 'fs';
 
-function findRegex(files, regex) {
-	const emitter = new EventEmitter();
-	// eslint-disable-next-line no-restricted-syntax
-	for (const file of files) {
-		readFile(file, 'utf8', (err, content) => {
-			if (err) {
-				return emitter.emit('error', err);
-			}
-
-			emitter.emit('fileread', file);
-			const match = content.match(regex);
-			if (match) {
-				match.forEach(elem => emitter.emit('found', file, elem));
-			}
-		});
+class FindRegex extends EventEmitter {
+	constructor(regex) {
+		super();
+		this.regex = regex;
+		this.files = [];
 	}
-	return emitter;
-}
 
-findRegex(
-	['exFiles/fileA.txt', 'exFiles/fileB.json'],
-	/hello \w+/g
-)
-	.on('fileread', file => console.log(`${file} was read`))
-	.on('found', (file, match) => console.log(`Matched "${match}" in ${file}`))
+	addFile(file) {
+		this.files.push(file);
+		return this;
+	}
+
+	find() {
+		// eslint-disable-next-line no-restricted-syntax
+		for (const file of this.files) {
+			readFile(file, 'utf8', (err, content) => {
+				if (err) {
+					return this.emit('error', err);
+				}
+				this.emit('fileread', file);
+				const match = content.match(this.regex);
+				if (match) {
+					match.forEach(elem => this.emit('found', file, elem));
+				}
+				return this;
+			});
+		}
+		return this;
+	}
+}
+const findRegexInstance = new FindRegex(/hello \w+/);
+findRegexInstance
+	.addFile('exfiles/fileA.txt')
+	.addFile('exfiles/fileB.json')
+	.find()
+	.on('found', (file, match) => console.log(`Matched "${match}" in file ${file}`))
 	.on('error', err => console.error(`Error emitted ${err.message}`));
