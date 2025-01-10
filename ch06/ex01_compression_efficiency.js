40743dfe3e8fd4672ab7653318e219198b6e62ed
Alex Walker <awalker@benaroyaresearch.org>
2025-01-10 13:16:10 -0800

    write to callback with runtime

--- ch06/ex01_compression_efficiency.js
+++ ch06/ex01_compression_efficiency.js
@@ -9,82 +9,91 @@ that we made some important performance considerations when we discussed it earl
 import { createGzip, createBrotliCompress, createDeflate } from 'zlib';
 import fs, { createReadStream, createWriteStream } from 'fs';
 import path from 'path';
-import { Transform, PassThrough } from 'stream';
+import { PassThrough } from 'stream';
 import { performance } from 'perf_hooks';
 
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const createCompressionTimer = name => {
+const createCompressionTimer = (name, completionCallback) => {
 	const monitor = new PassThrough();
-	const newTime = performance.now();
-	console.log(`do that thing at ${newTime}`);
 
 	let start;
 
 	// Capture start time when the first chunk is processed
 	monitor.on('pipe', () => {
-	  start = performance.now();
-	  console.log(`${name} compression started at ${start}ms`);
+		start = performance.now();
+		console.log(`${name} compression started at ${start} ms`);
 	});
 
 	monitor.on('finish', () => {
-	  const end = performance.now();
-	  console.log(`${name} step took ${end - start}ms. start = ${start}, newTime = ${newTime}`);
+		const end = performance.now();
+		const runTime = end - start;
+		console.log(`${name} step took ${runTime} ms. start = ${start}`);
+		if (completionCallback) {
+			completionCallback(runTime);
+		}
 	});
 
 	return monitor;
 };
 
-const compressFile = async (filePath, outputDir) => {
+const calculateCompressionPercentage = (originalSize, compressedSize) => {
+	if (!originalSize || originalSize === 0) {
+		console.warn('Original size must be greater than 0');
+		return null;
+	}
+	return ((1 - compressedSize / originalSize) * 100).toFixed(2);
+};
+
+const compressFile = async (filePath, outputDir, compressFileCallback) => {
 	const baseFilename = path.basename(filePath);
 	console.log(`baseFilename = ${baseFilename}`);
 	const outputPath = path.join(outputDir, baseFilename);
 	console.log(`baseFilename = ${baseFilename}, outputPath = ${outputPath}`);
 	const initSize = fs.statSync(filePath).size;
+	const compressionResults = {};
 
-	const compressionResults = {
-		gzip: {
-			initSize,
-		},
-	};
-
-
-	const compressionNamesOpts = {
+	const compressionOpts = {
 		gz: createGzip,
 		bz: createBrotliCompress,
 		def: createDeflate,
 	};
-	const gzTimer = createCompressionTimer('gz');
-	await sleep(3000); // Wait for one second
-	const gzipStream = createGzip()
-		.pipe(gzTimer)
-		.pipe(createWriteStream(`${outputPath}.gz`)).on('finish', () => {
-			console.log('gzip done.');
-		});
-
-	gzTimer.end();
-
-	const bzipStream = createBrotliCompress()
-		.pipe(createCompressionTimer('bz'))
-		.pipe(createWriteStream(`${outputPath}.bz`));
-	const deflateStream = createDeflate()
-		.pipe(createWriteStream(`${outputPath}.def`));
 
 	const inputStream = createReadStream(filePath);
-	inputStream
-		.pipe(gzipStream);
 
-	inputStream
-		.pipe(bzipStream);
+	const totalLength = Object.keys(compressionOpts).length;
+	let completed = 0;
+	Object.entries(compressionOpts).forEach(([ext, compressionFn]) => {
+		const outputFilePath = `${outputPath}.${ext}`;
+		const completionCallback = runtime => {
+			const finalSize = fs.statSync(outputFilePath).size;
+			const compressionPct = calculateCompressionPercentage(initSize, finalSize);
+			compressionResults[ext] = {
+				runtime,
+				finalSize,
+				compressionPct,
+			};
+		};
 
-	inputStream
-		.pipe(deflateStream);
+		const timer = createCompressionTimer(ext, completionCallback);
+		return inputStream.pipe(compressionFn())
+			.pipe(timer)
+			.pipe(createWriteStream(outputFilePath)).on('finish', () => {
+				completed += 1;
+				timer.end();
+				console.log(`finish for ${ext}. completed = ${completed}, totalLength = ${totalLength}`);
+				if (completed === totalLength) {
+					console.log('all set');
+					compressFileCallback(compressionResults);
+				}
+			});
+	});
 };
 
 const DEFAULT_DEST = '/tmp';
 const filePath = process.argv[2];
 const dest = process.argv[3] || DEFAULT_DEST;
 console.log(`filname = ${filePath}, dest = ${dest}`);
-compressFile(filePath, dest);
+
+const compressFileCallback = results => {
+	console.log(`results = ${JSON.stringify(results)}`);
+};
+compressFile(filePath, dest, compressFileCallback);
