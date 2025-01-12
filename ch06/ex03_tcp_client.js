e45061ceb2bf22dc50a988b9e332e0776a4e7341
Alex Walker <awalker@benaroyaresearch.org>
2025-01-12 13:42:01 -0800

    add enc to tcp transfer

--- ch06/ex03_tcp_client.js
+++ ch06/ex03_tcp_client.js
@@ -4,24 +4,42 @@ import { createReadStream } from 'fs';
 import { basename } from 'path';
 import { fork } from 'child_process';
 import { connect, createConnection } from 'net';
+import { createCipheriv, randomBytes } from 'crypto';
 
-const filePaths = process.argv.slice(2);
+const key = process.argv[2];
+if (!key || key.length !== 48) {
+	console.error('Error: The key must be a 48-character hexadecimal string.');
+	process.exit(1);
+}
 
+const filePaths = process.argv.slice(3);
+const secret = Buffer.from(key, 'hex');
+const iv = randomBytes(16);
 const SERVER_PORT = 9090;
+console.log(`iv = ${iv}`);
 
 filePaths.forEach(filePath => {
 	const client = createConnection(SERVER_PORT, 'localhost', () => {
 		console.log('Connected to server');
 		const filename = basename(filePath);
 		const filenameLength = Buffer.byteLength(filename);
-		const readStream = createReadStream(filePath);
+		const readStream = createReadStream(filePath)
+			.pipe(createCipheriv('aes192', secret, iv));
 
-		// Create metadata buffer
-		const metadata = Buffer.alloc(1 + 4 + filenameLength);
+		// Create metadata buffer (1 byte for filename length, filename length, and IV length)
+		const metadata = Buffer.alloc(1 + filenameLength + iv.length);
+		// filename length
 		metadata.writeUInt8(filenameLength, 0);
+
+		// filename
 		metadata.write(filename, 1, 'utf8');
-		metadata.writeUInt32BE(readStream.bytesRead, 1 + filenameLength);
+		// iv
+
+		iv.copy(metadata, 1 + filenameLength);
+
+		// Write the metadata to the server
 		client.write(metadata);
+
 		readStream.pipe(client);
 	});
 
--- ch06/ex03_tcp_server.js
+++ ch06/ex03_tcp_server.js
@@ -1,30 +1,37 @@
 import { createWriteStream } from 'fs';
 import { createServer } from 'net';
-import { createGunzip } from 'zlib';
 import { join } from 'path';
+import { createDecipheriv, randomBytes } from 'crypto';
 
 const SERVER_PORT = 9090;
 const OUTPUT_DIR = '/tmp';
+
+const secretServer = randomBytes(24);
+console.log(`Generated secret: ${secretServer.toString('hex')}`);
 
 const server = createServer(socket => {
 	console.log('Client connected');
 	let filename = null;
 	let fileStream = null;
-	let contentLength = null;
 
 	socket.on('data', chunk => {
 		if (!filename) {
 			// We need the filename length, filename, and content length
 			const filenameLength = chunk.readUInt8(0);
 			filename = chunk.toString('utf8', 1, 1 + filenameLength);
-			console.log(`Got data! file: ${filename}, filenameLength = ${filenameLength}`);
+			const iv = chunk.slice(1 + filenameLength, 1 + filenameLength + 16);
+
+			console.log(`Got data! file: ${filename}, filenameLength = ${filenameLength}. iv = ${iv}`);
 
 			const destFilename = join(OUTPUT_DIR, filename);
 			fileStream = createWriteStream(destFilename);
 
-			// ok, we've got the filname, so the data to write should be everything else
-			const remainingData = chunk.slice(1 + filenameLength + 4);
-			fileStream.write(remainingData);
+			// ok, we've got the filname, so the data to write should be everything else and decrypting
+			const decipher = createDecipheriv('aes192', secretServer, iv);
+
+			const remainingData = chunk.slice(1 + filenameLength + 16);
+			decipher.write(remainingData);
+			decipher.pipe(fileStream);
 		} else {
 			// Write file content
 			fileStream.write(chunk);
