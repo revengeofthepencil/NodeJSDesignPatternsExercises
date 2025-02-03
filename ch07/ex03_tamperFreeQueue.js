a7b148568adf3e59716dc828860fa9ed91eb6947
Alex Walker <awalker@benaroyaresearch.org>
2025-02-03 14:44:31 -0800

    ex03, immutable queue

--- ch07/ex03_tamperFreeQueue.js
+++ ch07/ex03_tamperFreeQueue.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 /*
 7.3 A tamper-free queue: Create a Queue class that has only one publicly accessible method called
 dequeue(). Such a method returns a Promise that resolves with a new element extracted from an
@@ -11,4 +13,67 @@ would push them into the queue. A loop would then consume all those messages usi
 method.
 */
 
-class TamperFreeQueue{};
+const MAX_QUEUE_COUNT = 10;
+const DATE_FORMAT_FOR_ENTRY_TIME = 'YYYY-MM-DD HH:mm:ss';
+
+class TamperFreeQueue {
+	constructor(items, executor) {
+		this.items = [...items];
+		this.waitingList = [];
+
+		const enqueue = item => {
+			if (this.waitingList.length > 0) {
+				// is anyone waiting?
+				const resolverFromWaitingList = this.waitingList.shift();
+				resolverFromWaitingList(item);
+			} else {
+				this.items.push(item);
+			}
+		};
+		executor(enqueue);
+	}
+
+	dequeue() {
+		return new Promise(resolve => {
+			if (this.items.length > 0) {
+				resolve(this.items.shift());
+			} else {
+				this.waitingList.push(resolve);
+			}
+		});
+	}
+}
+
+const waitOneToThreeSeconds = () => {
+	const delay = Math.floor(Math.random() * 2000) + 1000;
+	return new Promise(resolve => setTimeout(resolve, delay));
+};
+
+const getStringWithTime = () => {
+	const formattedTime = moment(new Date()).format(DATE_FORMAT_FOR_ENTRY_TIME);
+	return `Some text at ${formattedTime}`;
+};
+
+// yep, these should be the same
+const testString1 = getStringWithTime();
+const testString2 = getStringWithTime();
+const initItems = [testString1, testString2];
+
+const queue = new TamperFreeQueue(initItems, async enqueue => {
+	let queueCount = initItems.length;
+	while (queueCount < MAX_QUEUE_COUNT) {
+		// eslint-disable-next-line no-await-in-loop
+		await waitOneToThreeSeconds();
+		enqueue(getStringWithTime());
+		queueCount += 1;
+	}
+});
+
+let printedStringCount = 0;
+for (let i = 0; i < MAX_QUEUE_COUNT; i++) {
+	// eslint-disable-next-line no-loop-func
+	queue.dequeue().then(queueString => {
+		printedStringCount += 1;
+		console.log(`String #${printedStringCount} = '${queueString}'`);
+	});
+}
