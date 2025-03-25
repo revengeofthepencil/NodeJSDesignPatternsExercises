0738e7ce98eadbcbc9218da4494efb5e78a238fb
Alex Walker <awalker@benaroyaresearch.org>
2025-03-25 13:44:51 -0700

    proxy for console log

--- ch08/ex01.js
+++ ch08/ex01.js
@@ -1,3 +1,11 @@
+/*
+8.1 HTTP client cache: Write a proxy for your favorite HTTP client library
+that caches the response of a given HTTP request, so that if you make the
+same request again, the response is immediately returned from the local cache,
+rather than being fetched from the remote URL. If you need inspiration, you
+can check out the superagent-cache module (nodejsdp.link/superagent-cache).
+*/
+
 import fetch from 'node-fetch';
 
 const URL = 'https://randomuser.me/api/?nat=US&results=';
