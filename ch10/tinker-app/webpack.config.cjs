c4b0f910c40e38f928c441c47d66521b3540135f
Alex Walker <awalker@benaroyaresearch.org>
2025-05-04 18:50:47 -0700

    pull in webpack example using older versions

--- ch10/tinker-app/webpack.config.cjs
+++ ch10/tinker-app/webpack.config.cjs
@@ -1,40 +1,82 @@
-// webpack.config.js
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',  // your main JS file
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,  // cleans dist/ folder before each build
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,  // handle CSS imports
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,  // handle image imports
-        type: 'asset/resource',
-      },
-      {
-        test: /\.js$/,  // transpile JS
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    open: true,  // auto-open browser
-  },
-};
+const webpack = require('webpack');
 
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/*
+ * We've enabled HtmlWebpackPlugin for you! This generates a html
+ * page for you when you compile webpack, which will make you start
+ * developing and prototyping faster.
+ *
+ * https://github.com/jantimon/html-webpack-plugin
+ *
+ */
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
+
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				include: [path.resolve(__dirname, 'src')],
+				loader: 'babel-loader',
+
+				options: {
+					plugins: ['syntax-dynamic-import'],
+
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								modules: false,
+							},
+						],
+					],
+				},
+			},
+		],
+	},
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/,
+				},
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true,
+		},
+	},
+
+	devServer: {
+		open: true,
+	},
+};
