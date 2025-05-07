debf4dc78ce032f3ab649ea34e69e5e47af5ad03
Alex Walker <awalker@benaroyaresearch.org>
2025-05-07 16:37:03 -0700

    bail on old webpack exercises

--- ch10/tinker-app/src/say-hello.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import nunjucks from 'nunjucks';
-
-const template = '<h1>Hello <i>{{ name }}</i></h1>';
-export function sayHello(name) {
-	return nunjucks.renderString(template, { name });
-}
