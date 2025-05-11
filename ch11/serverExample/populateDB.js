6d64c65356579c0bc11e720146326300d321346d
Alex Walker <revengeofthepencil@gmail.com>
2025-05-10 19:12:12 -0700

    add d ex11

---
+++ ch11/serverExample/populateDB.js
@@ -0,0 +1,19 @@
+import { Level } from 'level';
+import { nanoid } from 'nanoid';
+
+const salesDb = new Level('example-db', { valueEncoding: 'json' });
+const products = ['book', 'game', 'app', 'song', 'movie'];
+
+async function populate() {
+	for (let i = 0; i < 100000; i++) {
+		// eslint-disable-next-line no-await-in-loop
+		await salesDb.put(nanoid(), {
+			amount: Math.ceil(Math.random() * 100),
+			product: products[Math.floor(Math.random() * 5)],
+		});
+	}
+
+	console.log('DB populated');
+}
+
+populate();
---
+++ ch11/serverExample/server.js
@@ -0,0 +1,16 @@
+import { createServer } from 'http';
+
+import { totalSales } from './totalSales.js';
+
+createServer(async (req, res) => {
+	const url = new URL(req.url, 'http://localhost');
+	const product = url.searchParams.get('product');
+	console.log(`Processing query: ${url.search}`);
+	const sum = await totalSales(product);
+	res.setHeader('Content-Type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify({
+		product,
+		sum,
+	}));
+}).listen(8000, () => console.log('Server started'));
