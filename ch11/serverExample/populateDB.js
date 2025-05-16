50a544c5167c8f37e002465f573b5239b1604e03
Alex Walker <awalker@benaroyaresearch.org>
2025-05-16 13:24:02 -0700

    add db count

--- ch11/serverExample/populateDB.js
+++ ch11/serverExample/populateDB.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-restricted-syntax */
 import { Level } from 'level';
 import { nanoid } from 'nanoid';
 
 const salesDb = new Level('example-db', { valueEncoding: 'json' });
 const products = ['book', 'game', 'app', 'song', 'movie'];
 
+async function countRecords() {
+	const keys = [];
+	for await (const key of salesDb.keys()) {
+		keys.push(key);
+	}
+	console.log(`Total records in DB: ${keys.length}`);
+	await salesDb.close();
+}
 async function populate() {
 	await salesDb.open(); // Ensure DB is open
-
+	await salesDb.clear(); // Clear existing records
 	for (let i = 0; i < 100000; i++) {
 		// eslint-disable-next-line no-await-in-loop
 		await salesDb.put(nanoid(), {
@@ -15,6 +24,7 @@ async function populate() {
 		});
 	}
 
+	await countRecords(); // Call the countRecords function after populating
 	console.log('DB populated');
 }
 
