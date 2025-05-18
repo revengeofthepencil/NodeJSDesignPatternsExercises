8bd7e4b33df9f97bf1b2bbc30fed15bc946ee4d2
Alex Walker <awalker@benaroyaresearch.org>
2025-05-18 10:19:43 -0700

    add cache to ex03

--- ch11/ex02_batching.js
+++ ch11/ex02_batching.js
@@ -9,9 +9,11 @@ import superagent from 'superagent';
 
 const baseUrl = 'http://localhost:8000';
 
-const products = ['book', 'game', 'app', 'song', 'movie'];
-
+const PRODUCT_KEYS = ['book', 'game', 'app', 'song', 'movie'];
+const CACHE_TTL = 7000;
 const runBatches = {};
+const cache = {};
+
 const runQuery = (product, callback) => {
 	const fullUrl = `${baseUrl}?product=${product}`;
 	if (!runBatches[product]) {
@@ -19,13 +21,19 @@ const runQuery = (product, callback) => {
 		superagent
 			.get(fullUrl)
 			.on('response', response => {
-				console.log(`response = ${JSON.stringify(response)}`);
-				console.log(`fullUrl = ${fullUrl} finished with ${runBatches[product].length} batches`);
 				const { body } = response;
 				const sum = body ? body.sum : 0;
-				runBatches[product].forEach(batchEntryCallback => {
+				cache[product] = sum;
+				setTimeout(() => {
+					cache[product] = null;
+				}, CACHE_TTL);
+
+				const callbacks = runBatches[product];
+				callbacks.forEach(batchEntryCallback => {
 					batchEntryCallback(sum);
 				});
+				// be sure to clear out the run batches so we can execute again
+				runBatches[product] = null;
 			})
 			.end((err, _) => {
 				if (err) {
@@ -36,21 +44,41 @@ const runQuery = (product, callback) => {
 		runBatches[product].push(callback);
 	}
 };
+const getCountForProduct = (product, callback) => {
+	if (cache[product] || cache[product] === 0) {
+		process.nextTick(() => {
+			callback(cache[product]);
+		});
+	} else {
+		runQuery(product, callback);
+	}
+};
 
-const runAllProducts = () => {
+const runAllProducts = runNumber => {
 	// do each 1-5 times
-	products.forEach(product => {
+	PRODUCT_KEYS.forEach(product => {
 		const random = Math.floor(Math.random() * 5) + 1;
-		console.log(`product = ${product}, random = ${random}`);
 		let runCount = 0;
 		while (runCount < random) {
 			runCount += 1;
 			const resultCallback = countForProduct => {
-				console.log(`count for ${product} and runCount = ${runCount} = ${countForProduct}`);
+				console.log(`count for ${product} with runNumber ${runNumber} and runCount = ${runCount} = ${countForProduct}`);
 			};
-			runQuery(product, resultCallback);
+			getCountForProduct(product, resultCallback);
 		}
 	});
 };
 
-runAllProducts();
+runAllProducts(1);
+
+console.log('wait 5 seconds for second run');
+setTimeout(() => {
+	console.log('kick off second run after 5 seconds');
+	runAllProducts(2);
+}, 5000);
+
+console.log('wait 10 seconds for third run');
+setTimeout(() => {
+	console.log('kick off third run after 10 seconds');
+	runAllProducts(3);
+}, 10000);
