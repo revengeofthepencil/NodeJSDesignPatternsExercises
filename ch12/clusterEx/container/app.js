ed947fbc8fe06db2551dec8d4bb83a4fa58c2833
Alex Walker <revengeofthepencil@gmail.com>
2025-05-25 15:40:32 -0700

    initial setup for ch12

---
+++ ch12/clusterEx/container/app.js
@@ -0,0 +1,9 @@
+import { createServer } from 'http';
+import { hostname } from 'os';
+
+const version = 1;
+const server = createServer((_, res) => {
+	let i = 1e7; while (i > 0) { i--; }
+	res.end(`Hello from ${hostname()} (v${version})`);
+});
+server.listen(8080);
