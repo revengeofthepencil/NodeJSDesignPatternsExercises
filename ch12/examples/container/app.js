767ca86d00819c994d67a7885a8e30aa714bc2f5
Alex Walker <awalker@benaroyaresearch.org>
2025-05-27 08:28:04 -0700

    re-name examples directory

--- ch12/examples/container/app.js
+++ ch12/examples/container/app.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http';
 import { hostname } from 'os';
 
-const version = 1;
+const version = 2;
 const server = createServer((_, res) => {
 	let i = 1e7; while (i > 0) { i--; }
 	res.end(`Hello from ${hostname()} (v${version})`);
